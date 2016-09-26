305cd989d2b839ba2b6db21e997f30b9a296a749
Tyler Abbot <tyler.abbot@sciencespo.fr>
2016-09-26 12:48:43 +0200

    Tylers comments on weeks 1-3.

--- HW3.R
+++ HW3.R
@@ -1,38 +1,42 @@
-#Homework 3 
+#Homework 3
 #Paul Bouche
 #21/09/16
 
 #Sorry I lacked of time...
 
 ###########
-#Question 1 
+#Question 1
 ###########
 
 #Documentation on functions
 
+############ Comment
+# Normally one puts the documentation before the function. Additionally
+# try to keep your documentation organized, eg giving standard
+# fields to fill such as Author, Title, Description, Inputs., etc.
 
-#Function 1 
+#Function 1
 
 add<- function(a, b){
   return (a+b)
 }
 
-#The function add() has two arguments, which are real numbers ("doubles" to be 
+#The function add() has two arguments, which are real numbers ("doubles" to be
 #precise). It returns the sum of those two numbers.
 
 
 
-#Function 2 
+#Function 2
 
 whereis <- function(pattern, word){
   return(grep(pattern, word))
 }
 
-#The two arguments of the function whereis() are characters. It uses the grep() 
+#The two arguments of the function whereis() are characters. It uses the grep()
 #function, that indicates the position of the first argument into the string of
 #caracters of the second one. The function returns this position.
 
-#Function 3 
+#Function 3
 
 our_sum <- function(x){
   summation <- 0
@@ -43,11 +47,11 @@ our_sum <- function(x){
 }
 
 #The function our_sum has only one argument: x, a vector of real numbers (doubles).
-#It consists into a loop on the values of this vector, summing each of them. 
+#It consists into a loop on the values of this vector, summing each of them.
 #Hence it returns the sum of the values of vector's elements.
 
 
-#Function 4 
+#Function 4
 
 F_inv_unif <- function(x, a, b) {
   x*(b - a) + a
@@ -57,15 +61,15 @@ F_inv_unif <- function(x, a, b) {
 
 #The function F_inv_univ has three arguments. The last ones, a and b, are real
 #numbers. The first one, x, is a vector of realizations of a uniform law on
-#interval [a,b]. 
-#Hence, the repartition funciton of each element of x takes the value : 
+#interval [a,b].
+#Hence, the repartition funciton of each element of x takes the value :
 #(x-a)/(b-a).
-#Therefore, image of x by F_inv_unif is a set of #x realizations of a 
+#Therefore, image of x by F_inv_unif is a set of #x realizations of a
 #uniform law on [0,1]
 
 
 
-#Function 5 
+#Function 5
 
 rand_sample <- function(n, F_inv=function(x) x){
   unif_sample <- runif(n)
@@ -74,73 +78,3 @@ rand_sample <- function(n, F_inv=function(x) x){
 
 #The function rand_sample has two arguments: an integer n and a function F_inv.
 #It returns a set of n realizations of a uniform law on [0,1]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
